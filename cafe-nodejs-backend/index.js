52983daf1a2feaacb6f4cd47c5fcc53f94cd24bb
Hariom Patel <hariom70002582@gmail.com>
2026-06-27 07:53:09 +0530

    feat: initialize full-stack cafe management system with angular frontend and nodejs backend project structure

---
+++ cafe-nodejs-backend/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const connection = require('./connection');
+const userRoutes = require('./routes/user.routes');
+
+const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/user',userRoutes);
+    
+module.exports = app;
---
+++ cafe-nodejs-backend/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const app = require('./index');
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
