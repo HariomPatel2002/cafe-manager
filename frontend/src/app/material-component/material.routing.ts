1f42ea3030c2a588879ba975a281c5e63b281f49
Hariom Patel <hariom70002582@gmail.com>
2026-07-22 09:03:01 +0530

    feat: implement product management module with services, dialogs, and table components

--- frontend/src/app/material-component/material.routing.ts
+++ frontend/src/app/material-component/material.routing.ts
@@ -1,10 +1,19 @@
 import { Routes } from '@angular/router';
 import { ManageCategoryComponent } from './manage-category/manage-category.component';
 import { RouteGuardService } from '../services/route-guard.service'
+import { ManageProductComponent } from './manage-product/manage-product.component';
 export const MaterialRoutes: Routes = [
     {
         path: 'category',
         component: ManageCategoryComponent,
+        canActivate: [RouteGuardService],
+        data: {
+            expectedRole: ['admin']
+        }
+    },
+    {
+        path: 'product',
+        component: ManageProductComponent,
         canActivate: [RouteGuardService],
         data: {
             expectedRole: ['admin']
